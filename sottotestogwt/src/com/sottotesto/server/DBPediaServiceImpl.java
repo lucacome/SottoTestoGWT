4140cb357e99352ae8555e41ef9a1d6a24952112
Luca Comellini <luca.com@gmail.com>
2013-06-07 12:59:29 +0200

    dbpedia

---
+++ sottotestogwt/src/com/sottotesto/server/DBPediaServiceImpl.java
@@ -0,0 +1,62 @@
+package com.sottotesto.server;
+
+import com.google.gson.Gson;
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.query.ResultSetFormatter;
+import com.sottotesto.client.GeneralService;
+import com.sottotesto.server.TagmeDataWrapper.TagmeData;
+import com.sottotesto.shared.Debug;
+
+public class DBPediaServiceImpl extends RemoteServiceServlet implements GeneralService {
+	
+	public String sendToServer(String input) throws IllegalArgumentException {
+		Debug.printDbgLine("DBPediServiceImpl.java: sendToServer()");
+		//Debug.writeDbgLine(request.getAttribute("responsetag").toString());
+
+	
+		TagmeData data = null;
+		Gson gson = new Gson();
+		String responsetag = input;
+				//request.getAttribute("responsetag").toString();
+		data = gson.fromJson(responsetag, TagmeData.class);
+		
+		ResultSet results = null;
+		String resultquery = "";
+		for (int i=0; i<=data.annotations.size()-1; i++){
+			String titletag = data.annotations.get(i).title;	
+		
+		
+		titletag = titletag.replaceAll(" ", "_");
+		
+	    String s2 = "PREFIX  dbpprop: <http://dbpedia.org/property/>\n" +
+	    		"\n" +
+	    		"SELECT  *\n" +
+	            "WHERE {\n" +
+	            "<http://dbpedia.org/resource/" + titletag + "> dbpprop:placeOfBirth ?nat .\n" +
+	            "  }\n" +
+	            "";
+
+	    Query query2 = QueryFactory.create(s2); //s2 = the query above
+	    QueryExecution qExe = QueryExecutionFactory.sparqlService( "http://dbpedia.org/sparql", query2 );
+	    results = qExe.execSelect(); 
+	    resultquery += ResultSetFormatter.asXMLString(results);
+//	    ResultSetFormatter.outputAsJSON(results);
+	        
+//	    resultquery += results;
+		}
+//	    String temp = String.valueOf(data.annotations.size());
+		
+		//request.setAttribute("dbpOutput", resultquery);  
+//		request.setAttribute("hello_string", titletag);  
+
+		//request.getRequestDispatcher("/dbpedia.jsp").forward(request, response);
+		
+		return resultquery;
+		
+	}
+}
