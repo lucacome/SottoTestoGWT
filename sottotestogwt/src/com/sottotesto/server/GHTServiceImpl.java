9e30714e68c511de73fb2e27ede1900ebb19b34a
He4dShOt <luca.com@gmail.com>
2014-01-10 19:15:36 +0100

    GHT che sembra andare

--- sottotestogwt/src/com/sottotesto/server/GHTServiceImpl.java
+++ sottotestogwt/src/com/sottotesto/server/GHTServiceImpl.java
@@ -1,13 +1,16 @@
 package com.sottotesto.server;
 
+import java.util.Iterator;
 import java.util.List;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonParser;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import com.sottotesto.client.GHTService;
 
 public class GHTServiceImpl extends RemoteServiceServlet implements GHTService	{
-
-
 
 	/**
 	 * 
@@ -16,8 +19,39 @@ public class GHTServiceImpl extends RemoteServiceServlet implements GHTService	{
 
 	public String sendToServer(String jsonHT, List<String> selectedLink) throws IllegalArgumentException {
 		String response="";
-		
-		
+
+
+
+		Gson pa = new Gson();
+		JsonParser parser = new JsonParser();
+		Gson aa = new GsonBuilder().disableHtmlEscaping().create();
+
+		JsonArray array = parser.parse(jsonHT).getAsJsonArray();
+
+		JData jd = pa.fromJson(array.get(0), JData.class);
+		response += aa.toJson(jd)+ ",";
+
+		for ( int i=1; i<array.size(); i++){
+			JData jdata = new JData();
+			jdata =	pa.fromJson(array.get(i), JData.class);
+			Iterator<String> linkiter = selectedLink.listIterator();
+			while(linkiter.hasNext())
+				if (jdata.id.contains(linkiter.next())){
+					response += aa.toJson(jdata) + ",";
+					for (int j = i +1 ; j<array.size(); j++){
+						jdata =	pa.fromJson(array.get(j), JData.class);
+						if (jdata.id.contains("http"))
+							response += aa.toJson(jdata) + ",";
+						else{
+							i=j;
+							break;
+						}
+					}
+				}
+		}
+		response = "[" + response.substring(0, response.length()-1) + "]";
+		//Debug.printDbgLine(response);
+
 		return response;
 	}
 
